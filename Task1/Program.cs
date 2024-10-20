ab9087c16070fdb1a18d0c10249ae86b8eaa300e
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-10-20 21:15:29 +0200

    Initialize .NET Core project with basic structure
    Add essential files and configurations for a new .NET Core 3.1 project. Includes project files, assembly info, and initial 'Hello World!' program.

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learning_OOP_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
