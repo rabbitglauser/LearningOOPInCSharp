5b0d4ee4c081b5784049cf532d419fe3ca878076
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-10-20 22:13:41 +0200

    Add Computer class with a constructor
    Introduced a new `Computer` class with properties `color`, `model`, and `brand`, and defined a constructor to initialize these properties. Also added a `Program` class to instantiate a `Computer` object and display its attributes.

---
+++ taks3/Constructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Computer
+{
+  public string color;
+  public string model;
+  public string brand;
+
+  public Computer(string computerColor, string computerModel, string computerBrand)
+  {
+    color = computerColor;
+    model = computerModel;
+    brand = computerBrand;
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Computer mycomputer = new Computer("Red", "Sedan", "Toyota");
+    Console.WriteLine($"Car color: {mycomputer.color}, Model: {mycomputer.model}, brand: {mycomputer.brand}");
+  }
+}
