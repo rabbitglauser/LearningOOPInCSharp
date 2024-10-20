e57a45c393d86dbc49bcd4795e38e1d5a34b9818
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-10-20 22:39:58 +0200

    Add new class to handle person input and output
    Implemented a new class in `task4/class.cs` to read and display person names. Added an entry point with a `Main` method to create an array of `Person` objects, populate them with user input, and print their names to the console.

---
+++ task4/class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ // The error "too many entry points defined" occurs because you have more than one Main method across multiple classes, and the compiler doesn’t know which one to use as the entry point for the program.
+//my answer main is only defined in one place?
+public class FirstClass
+{
+    public static void Main(string[] args)
+    {
+        int total = 3;
+        Person[] persons = new Person[total];
+ 
+        for (int i = 0; i < total; i++)
+        {
+            persons[i] = new Person() {
+                Name = Console.ReadLine()
+            };
+        }
+ 
+        for (int i = 0; i < total; i++)
+        {
+            Console.WriteLine(persons[i].ToString());
+        }
+ 
+        Console.ReadLine();
+    }
+ 
+    public class Person
+    {
+        public string Name { get; set; }
+ 
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+    }
+}
